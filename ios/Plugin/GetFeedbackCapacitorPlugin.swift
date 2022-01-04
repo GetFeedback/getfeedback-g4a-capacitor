11def6e61200c3caff9c35e7003215bb6c055718
Hitesh Jain <jainhitesh999@gmail.com>
2022-01-04 10:33:11 +0100

    v1.0.0

---
+++ ios/Plugin/GetFeedbackCapacitorPlugin.swift
@@ -0,0 +1,151 @@
+import Foundation
+import Capacitor
+import Usabilla
+import UIKit
+
+/**
+ * Please read the Capacitor iOS Plugin Development Guide
+ * here: https://capacitorjs.com/docs/plugins/ios
+ */
+@objc(GetFeedbackCapacitorPlugin)
+public class GetFeedbackCapacitorPlugin: CAPPlugin {
+    var passiveCallID: String?
+    var campaignCallID: String?
+
+    let getfeedback = Usabilla.self
+    override public func load() {
+        getfeedback.delegate = self
+    }
+
+    @objc weak var formNavigationController: UINavigationController?
+
+    @objc func initialize(_ call: CAPPluginCall) {
+        let appID = call.getString("appID") ?? ""
+        getfeedback.initialize(appID: appID)
+    }
+    
+    @objc func loadFeedbackForm(_ call: CAPPluginCall) {
+        let formID = call.getString("formID") ?? ""
+        self.bridge?.saveCall(call)
+        passiveCallID = call.callbackId
+        getfeedback.loadFeedbackForm(formID)
+    }
+    
+    @objc func loadFeedbackFormWithCurrentViewScreenshot(_ call: CAPPluginCall) {
+        let formID = call.getString("formID") ?? ""
+        self.bridge?.saveCall(call)
+        passiveCallID = call.callbackId
+        DispatchQueue.main.sync {
+            if let rootVC = UIApplication.shared.windows.first?.rootViewController {
+              let screenshot = self.takeScreenshot(view: rootVC.view)
+                getfeedback.loadFeedbackForm(formID, screenshot: screenshot)
+          }
+        }
+    }
+    
+    @objc func preloadFeedbackForms(_ call: CAPPluginCall) {
+        guard let formIDs = call.options["formIDs"] as? [String] else {
+          call.reject("Must provide formIDs")
+          return
+        }
+        getfeedback.preloadFeedbackForms(withFormIDs: formIDs)
+    }
+    
+    @objc func removeCachedForms(_ call: CAPPluginCall) {
+        getfeedback.removeCachedForms()
+    }
+    
+    @objc func sendEvent(_ call: CAPPluginCall) {
+        let eventName = call.getString("eventName") ?? ""
+        self.bridge?.saveCall(call)
+        campaignCallID = call.callbackId
+        getfeedback.sendEvent(event: eventName)
+    }
+    
+    @objc func setCustomVariables(_ call: CAPPluginCall) {
+        guard let variables = call.options["customVariables"] as? [String: String] else {
+            call.reject("Must provide variables")
+            print("ERROR : Expected customVariables as Dictionary of String [String : String]")
+            return
+        }
+        getfeedback.customVariables = variables
+    }
+    
+    @objc func setDebugEnabled(_ call: CAPPluginCall) {
+        guard let debugEnabled = call.options["debugEnabled"] as? Bool else {
+            call.reject("Must provide debugEnabled")
+            return
+        }
+        getfeedback.debugEnabled = debugEnabled
+    }
+    
+    @objc func loadLocalizedStringFile(_ call: CAPPluginCall) {
+        let localizedStringFile = call.getString("localizedStringFile") ?? ""
+        getfeedback.localizedStringFile = localizedStringFile
+    }
+    
+    func takeScreenshot(view: UIView) -> UIImage {
+        return getfeedback.takeScreenshot(view)!
+    }
+    
+    @objc func resetCampaignData(_ call: CAPPluginCall) {
+        getfeedback.resetCampaignData {
+        }
+    }
+
+    @objc func dismiss(_ call: CAPPluginCall) {
+        let _ = getfeedback.dismiss()
+    }
+    
+    @objc func getDefaultDataMasks(_ call: CAPPluginCall) {
+        let str = getfeedback.defaultDataMasks
+        call.resolve(["DEFAULT_DATA_MASKS": str])
+    }
+    
+    @objc func setDataMasking(_ call: CAPPluginCall) {
+        let masks = call.options["masks"] as? [String] ?? getfeedback.defaultDataMasks
+        let maskChar = call.getString("character")?.first ?? "X"
+        Usabilla.setDataMasking(masks: masks, maskCharacter: maskChar)
+    }
+}
+
+extension GetFeedbackCapacitorPlugin: UsabillaDelegate {
+    public func formDidLoad(form: UINavigationController) {
+        formNavigationController = form
+        if let rootVC = UIApplication.shared.keyWindow?.rootViewController {
+            rootVC.present(formNavigationController!, animated: true, completion: nil)
+        }
+    }
+    
+    public func formDidFailLoading(error: UBError) {
+        formNavigationController = nil
+         if let callID = passiveCallID, let call = bridge?.savedCall(withID: callID) {
+            call.reject("The form could not be loaded")
+            bridge?.releaseCall(call)
+        }
+    }
+    
+    public func formDidClose(formID: String, withFeedbackResults results: [FeedbackResult], isRedirectToAppStoreEnabled: Bool) {
+            var rnResults: [[String : Any]] = []
+            for result in results {
+                let dictionary: Dictionary = ["rating": result.rating ?? 0, "abandonedPageIndex": result.abandonedPageIndex ?? 0, "sent": result.sent] as [String : Any]
+                rnResults.append(dictionary)
+            }
+            formNavigationController = nil
+        
+        if let callID = passiveCallID, let call = bridge?.savedCall(withID: callID) {
+            call.resolve(["formId": formID, "results": rnResults, "isRedirectToAppStoreEnabled": isRedirectToAppStoreEnabled])
+            bridge?.releaseCall(call)
+        }
+    }
+        
+    public func campaignDidClose(withFeedbackResult result: FeedbackResult, isRedirectToAppStoreEnabled: Bool) {
+            let rnResult: [String : Any] = ["rating": result.rating ?? 0, "abandonedPageIndex": result.abandonedPageIndex ?? 0, "sent": result.sent] as [String : Any]
+            formNavigationController = nil
+        
+        if let callID = campaignCallID, let call = bridge?.savedCall(withID: callID) {
+            call.resolve(["result": rnResult, "isRedirectToAppStoreEnabled": isRedirectToAppStoreEnabled])
+            bridge?.releaseCall(call)
+        }
+    }
+}
---
+++ src/web.ts
@@ -0,0 +1,56 @@
+import { WebPlugin } from '@capacitor/core';
+
+import type { GetFeedbackCapacitorPlugin } from './definitions';
+
+export class GetFeedbackCapacitorWeb
+  extends WebPlugin
+  implements GetFeedbackCapacitorPlugin {
+  initialize(options: { appID: string; }): void {
+    console.log('initialize', options);
+    throw new Error('Method not implemented.');
+  }
+  loadFeedbackForm(options: { formID: string; }): Promise<any> {
+    console.log('loadFeedbackForm', options);
+    throw new Error('Method not implemented.');
+  }
+  sendEvent(options: { eventName: string; }): Promise<any>  {
+    console.log('sendEvent', options);
+    throw new Error('Method not implemented.');
+  }
+  resetCampaignData(): void {
+    throw new Error('Method not implemented.');
+  }
+  removeCachedForms(): void {
+    throw new Error('Method not implemented.');
+  }
+  setDebugEnabled(options: { debugEnabled: boolean; }): void {
+    console.log('setDebugEnabled', options);
+    throw new Error('Method not implemented.');
+  }
+  loadLocalizedStringFile(options: { localizedStringFile: string; }): void {
+    console.log('loadLocalizedStringFile', options);
+    throw new Error('Method not implemented.');
+  }
+  loadFeedbackFormWithCurrentViewScreenshot(options: { formID: string; }): Promise<any>  {
+    console.log('loadFeedbackFormWithCurrentViewScreenshot', options);
+    throw new Error('Method not implemented.');
+  }
+  preloadFeedbackForms(options: { formIDs: string[]; }): void {
+    console.log('preloadFeedbackForms', options);
+    throw new Error('Method not implemented.');
+  }
+  setCustomVariables(options: { customVariables: any; }): void {
+    console.log('setCustomVariables', options);
+    throw new Error('Method not implemented.');
+  }
+  dismiss(): void {
+    throw new Error('Method not implemented.');
+  }
+  setDataMasking(options: { masks: string[]; character: string; }): void {
+    console.log('setDataMasking', options);
+    throw new Error('Method not implemented.');
+  }
+  getDefaultDataMasks(): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+}
